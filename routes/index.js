c3f55306d4d7a9b2ab86f3f927ca4c92abce8fbb
Simon Frieling <simon.frieling@avaloq.com>
2020-03-30 18:11:37 +0200

    route to react app

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ response: "I am alive" }).status(200);
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 module.exports = router;
