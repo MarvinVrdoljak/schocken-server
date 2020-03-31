32f1f1c45e31b067debf684c8d2eb98cbd9d08e6
Marvin Vrdoljak <marvin.vrdoljak@dudagroup.com>
2020-03-31 20:15:39 +0200

    variablen fusioniert

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,14 @@ import "./App.css";
 const socket = require('socket.io-client/dist/socket.io')("http://env-0915955.hidora.com:4001");
 
 function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [half, setHalf] = useState(1);
-  const [isHalfDone, setIsHalfDone] = useState(false);
-  const [batches, setBatches] = useState(13);
+  const [game, setGame] = useState({
+    modalIsOpen: false,
+    half: 1,
+    isHalfDone: false,
+    batches: 13,
+    playerId: 100
+  })
+
   const [players, setPlayers] = useState([
     { id: 1,
       name: "Marvin", round: 0, batch: 0, loser: false, dices: [
@@ -31,8 +35,6 @@ function App() {
     },
   ]);
 
-  const [playerId, setPlayerId] = useState(players.length + 1);
-
     socket.on('connect', function(data) {
         console.log("Connected...");
         socket.emit("roll", players);
@@ -43,11 +45,11 @@ function App() {
    });
 
 
-   function updateModalStatus(){
-    // setModalIsOpen(!modalIsOpen)
-    setModalIsOpen(!modalIsOpen);
-  }
 
+
+   function updateModalStatus(){
+    setGame({...game, modalIsOpen: !game.modalIsOpen});
+  }
 
    function updatePlayerDices(id, dices, addRound){
      setPlayers(
@@ -70,24 +72,23 @@ function App() {
   }
 
   function addPlayer(name) {
-    setPlayers([...players, {id: playerId, name, round: 0, batch: 0, loser: false, dices: [
+    setPlayers([...players, {id: game.playerId, name, round: 0, batch: 0, loser: false, dices: [
       {id: 1, value: 1, selected: false, visible: true},
       {id: 2, value: 2, selected: false, visible: true},
       {id: 3, value: 3, selected: false, visible: true},
     ]}]);
-    setPlayerId(playerId + 1);
+    setGame({...game, playerId: players.length + 1});
   }
 
   function addBatch(id){
     setPlayers(
       players.map(player => {
-        if (player.id === id && batches > 0) {
+        if (player.id === id && game.batches > 0) {
           player.batch =  player.batch + 1;
-          setBatches(batches - 1);
           if (player.batch === 13){
-            setIsHalfDone(true);
+            setGame({...game, isHalfDone: true, batches: game.batches - 1});
           } else{
-            setIsHalfDone(false);
+            setGame({...game, isHalfDone: false, batches: game.batches - 1});
           }
         }
         return player;
@@ -100,7 +101,7 @@ function App() {
       players.map(player => {
         if (player.id === id && player.batch >= 1) {
           player.batch = player.batch - 1;
-          setBatches(batches + 1);
+          setGame({...game, batches: game.batches + 1});
         }
         return player;
       })
@@ -129,26 +130,25 @@ function App() {
   }
 
   function resetHalf(){
-    if (half === 1 || half === 2){
+    if (game.half === 1 || game.half === 2){
       players.map(player => {
         if (player.batch === 13 && player.loser === false){
           player.loser = true;
-          setHalf(half + 1);
           resetRound(true, false);
+          setGame({...game, setIsHalfDone: false, batches: 13, half: game.half + 1});
         } else if (player.batch === 13 && player.loser === true){
           window.confirm('Da hat wohl ein Noob direkt 2x verloren! Ihr beginnt direkt ein neues Spiel!')
-          setHalf(1);
+          // setHalf(1);
+          setGame({...game, setIsHalfDone: false, batches: 13, half: 1});
           resetRound(true, true);
         }
         return player;
       })
 
     } else{
-      setHalf(1);
       resetRound(true, true);
+      setGame({...game, setIsHalfDone: false, batches: 13, half: 1});
     }
-    setBatches(13);
-    setIsHalfDone(false);
   }
 
   return (
@@ -156,21 +156,21 @@ function App() {
       <header className="header">
           <h1>Schocoronia <img className="header__info" src={infoIcon} alt="Wichtige Hinweise" onClick={e => updateModalStatus()}/></h1>
           <AddPlayer addPlayer={addPlayer}/>
-          <p><small><strong>Hälfte: { half !== 3 ? half : 'Loser-Round' } // Verbleibende Batches: {batches}</strong></small></p>
+          <p><small><strong>Hälfte: { game.half !== 3 ? game.half : 'Loser-Round' } // Verbleibende Batches: {game.batches}</strong></small></p>
       </header>
       <main className="board" style={{backgroundImage:  `url(${bg})`}}>
-        <Players players={players} batches={batches} updatePlayerDices={updatePlayerDices} deletePlayer={deletePlayer} addBatch={addBatch} removeBatch={removeBatch} />
+        <Players players={players} batches={game.batches} updatePlayerDices={updatePlayerDices} deletePlayer={deletePlayer} addBatch={addBatch} removeBatch={removeBatch} />
         <div className="board__buttons">
           <button className="button" onClick={resetRound}>Runde neu Starten</button>
-          <button className={"button" + (isHalfDone ? '' : ' disable')} onClick={resetHalf}>
-            {half === 1 ? '2. Hälfte Starten' : ''}
-            {half === 2 ? 'Verlierer ausspielen' : ''}
-            {half === 3 ? 'Neues Spiel' : ''}
+          <button className={"button" + (game.isHalfDone ? '' : ' disable')} onClick={resetHalf}>
+            {game.half === 1 ? '2. Hälfte Starten' : ''}
+            {game.half === 2 ? 'Verlierer ausspielen' : ''}
+            {game.half === 3 ? 'Neues Spiel' : ''}
           </button>
         </div>
       </main>
-      <Footer modalIsOpen={modalIsOpen} updateModalStatus={updateModalStatus}/>
-      <Information modalIsOpen={modalIsOpen} updateModalStatus={updateModalStatus}/>
+      <Footer modalIsOpen={game.modalIsOpen} updateModalStatus={updateModalStatus}/>
+      <Information modalIsOpen={game.modalIsOpen} updateModalStatus={updateModalStatus}/>
     </div>
   );
 }
