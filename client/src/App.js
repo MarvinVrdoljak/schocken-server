b0842b44a0a551f8f5241fbe8abf4d666b90dea7
Simon Frieling <simon.frieling@avaloq.com>
2020-04-01 12:47:07 +0200

    Testing

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import bg from "./images/board.jpg";
 import infoIcon from "./images/info.svg";
 
 import "./App.css";
-const socket = require('socket.io-client/dist/socket.io')();//"http://env-0915955.hidora.com:4001"
+const socket = require('socket.io-client/dist/socket.io')("env-0915955.hidora.com:4001");
 
 function App() {
   const [game, setGame] = useState({
