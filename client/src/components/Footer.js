ffc9c1a129405b78cda402f69ba3ff1c20237f63
Marvin Vrdoljak <marvin.vrdoljak@dudagroup.com>
2020-04-01 09:03:44 +0200

    fixes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,8 @@ function Footer(state) {
     <footer className="footer">
       <a href={"#/"} alt="Wichtige Hinweise" onClick={() => state.updateModalStatus()}>Informationen</a>
       &nbsp;|&nbsp;
+      <a href={"https://www.spielregeln.de/schocken.html"} alt="Spenden" target="_blank">Spielregeln</a>
+      &nbsp;|&nbsp;
       <a href={"https://www.betterplace.org/de/fundraising-events/34641-schocoronia-fur-die-arche-kinderstiftung"} alt="Spenden" target="_blank">Jetzt Spenden</a>
     </footer>
   );
